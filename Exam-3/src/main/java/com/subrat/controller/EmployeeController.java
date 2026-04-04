44571cbcacf64c3173181433f90af17d65054438
subrat naik <subratnaik2008@gmail.com>
2026-04-04 23:32:58 +0530

    practice

--- Exam-3/src/main/java/com/subrat/controller/EmployeeController.java
+++
@@ -1,8 +0,0 @@
-package com.subrat.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller("ec")
-public class EmployeeController {
-
-}
--- Exam/src/main/java/com/subrat/runner/Runner.java
+++
@@ -1,8 +0,0 @@
-package com.subrat.runner;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Runner {
-
-}
